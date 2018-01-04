25a4833d03a2d40856023f4967516191acc0e18c
Kiran Balakrishnan <kiran.b@acelrtech.com>
2018-01-04 13:40:35 +0530

    scrape

--- selenium/scrape.py
+++ selenium/scrape.py
@@ -5,5 +5,11 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get("https://www.justdial.com/Bangalore/Car-Washing-Services/nct-10079017")
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
+parse_page(driver)
+
+
+def parse_page(driver):
+	links = driver.find_elements_by_class_name('tabsM').find_elements_by_tag_name('a')
+
 #result = get_num_from_screenshot("") 
